18c46a33e8661675a9dee8096160c839dd32ee3d
Alexey Murz Korepov <MurzNN@gmail.com>
2023-06-08 08:18:47 +0400

    Initial

---
+++ apply.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# LANG=C sudo ansible-playbook ./murz-env.yaml -e ansible_python_interpreter=/usr/bin/python3
+ansible-galaxy install -r requirements.yml
+ansible-playbook ./murz-workstation.yml
