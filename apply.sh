0a7ae7db691573a7e9048428f1b0abd2063f15ed
Alexey Murz Korepov <MurzNN@gmail.com>
2023-06-10 11:14:55 +0400

    Ask become pass

--- apply.sh
+++ apply.sh
@@ -2,4 +2,4 @@
 
 # LANG=C sudo ansible-playbook ./murz-env.yaml -e ansible_python_interpreter=/usr/bin/python3
 ansible-galaxy install -r requirements.yml
-ansible-playbook ./murz-workstation.yml
+ansible-playbook ./murz-workstation.yml --ask-become-pass
