0ead5cb7c8be38e5698e549da9ad7ce78364d10e
Alexey Murz Korepov <MurzNN@gmail.com>
2023-06-11 09:54:43 +0400

    Add packages from ppa

--- apply.sh
+++ apply.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # LANG=C sudo ansible-playbook ./murz-env.yaml -e ansible_python_interpreter=/usr/bin/python3
-ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yml && \
 ansible-playbook ./murz-workstation.yml --ask-become-pass
