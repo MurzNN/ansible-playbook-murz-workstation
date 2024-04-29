e77e90107ce43c725c4a158db4ee70db6c0934ad
Alexey Murz Korepov <MurzNN@gmail.com>
2024-04-29 16:43:25 +0400

    temp directory cron, ppas

--- apply.sh
+++ apply.sh
@@ -2,4 +2,4 @@
 
 # LANG=C sudo ansible-playbook ./murz-env.yaml -e ansible_python_interpreter=/usr/bin/python3
 ansible-galaxy install -r requirements.yml && \
-ansible-playbook ./murz-workstation.yml --ask-become-pass
+ansible-playbook ./murz-workstation.yml --ask-become-pass --extra-vars "ansible_config_user=$USER"
